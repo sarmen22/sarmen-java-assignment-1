b8e4e0967515a27643df3940a442be533baf1e7e
Sarmen Pirageneh <sarmen22@gmail.com>
2023-02-05 02:31:37 -0800

    Final Assignment 1

--- src/com/sarmen/assignment1/Assignment1Application.java
+++ src/com/sarmen/assignment1/Assignment1Application.java
@@ -1,9 +1,37 @@
 package com.sarmen.assignment1;
 
+import java.util.Scanner;
+
 public class Assignment1Application {
 
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World !!!!");
+		Scanner scanner = new Scanner(System.in);
+		
+		double initinvest = CollectInput(scanner , "Type in your starting investment: ");
+		
+		double investrate = CollectInput(scanner , "Type in your interest rate: ");
+		
+		double newBalance = initinvest;
+		
+		for (int i = 5;i <= 30; i = i + 5) {
+			double result = CalculateInvest(newBalance, investrate, i);
+			System.out.println("After "+ i + " years, you would have: " + Math.round(result));
+		}	
+
+	}
+	
+	private static double CollectInput(Scanner scanner, String messagetodisplay) {
+		System.out.println(messagetodisplay);
+		return scanner.nextDouble();
+		
+	}
+	
+	private static double CalculateInvest(double newBalance , double investrate , int year) {
+		for(int i=1;i<= year;i++) {
+			newBalance = newBalance * (investrate + 1.0);
+		}
+		return newBalance;
 	}
 
 }
